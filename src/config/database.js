591ecfa0a06d0a69e2085a368a32528ce2127115
andresandaar <91690152+andresandaar@users.noreply.github.com>
2021-11-09 14:54:24 -0500

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+	/* 	url: 'mongodb://localhost/login-node'  */
+	/* base de datos local:
+   si quires que tus datos se guaraden el mongodb local, debes de tener activo tu servidor mongod para que te pueda funcionar */
+
+	/* pero si quires usar el servicio en la nuve mongodb atlas  simplemente necesitas tener la cuenta atlas*/
+	url: 'mongodb+srv://andres:<aqui va la contraseña>@cluster0.yamwg.mongodb.net/login-node?retryWrites=true&w=majority',
+};
